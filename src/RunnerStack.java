14d49a1c413ac7cb2b0fe2f66c498611fb6dd823
Mayur2111995 <barimayur1265@gmail.com>
2022-06-21 12:15:44 +0530

    Added:LinkedList

---
+++ src/RunnerStack.java
@@ -0,0 +1,12 @@
+public class RunnerStack {
+    public static void main(String[] args) {
+        MyStack<Integer> List = new MyStack<Integer>();
+        List.push(70);
+        List.push(30);
+        List.push(56);
+        List.showRunner();
+
+
+    }
+}
+
